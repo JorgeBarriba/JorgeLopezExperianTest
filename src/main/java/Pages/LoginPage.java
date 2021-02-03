803705085f9d8cf90eaec4e4e41c956ff90d9b42
Jorsh <jorgebarriba@gmail.com>
2021-02-03 16:38:42 -0600

    -Buscar los mejores selectores para los objetos, evitar xpath absolutos -Separar los datos del codigo

--- src/main/java/Pages/LoginPage.java
+++ src/main/java/Pages/LoginPage.java
@@ -11,24 +11,28 @@ import java.util.List;
 
 public class LoginPage {
     private WebDriver driver;
-    //private static String pageUrl = "https://todoist.com/";
+
 
     public LoginPage(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
     //Locators
-    @FindBy(xpath="//*[@id='__next']/div/main/div[1]/header/nav/div/ul[2]/li[1]/a") private WebElement login_button;
-    @FindBy(id="email") private WebElement email_text; //jorgebarriba@gmail.com
-    @FindBy(id="password") private WebElement password_text; //Challenge02
-    @FindBy(xpath="//*[@id='login_form']/button") private WebElement session_button;
+    @FindBy(how= How.PARTIAL_LINK_TEXT, using = "Inicia") private WebElement login_button;
+    @FindBy(className="submit_btn") private WebElement session_button;
+    @FindBy(className="error_msg") private WebElement error_login_message;
+    @FindBy(id="email") private WebElement email_text;
+    @FindBy(id="password") private WebElement password_text;
     @FindBy(how= How.CLASS_NAME, using = "plus_add_button") private WebElement plus_add_button;
     @FindBy(how= How.CLASS_NAME, using = "public-DraftStyleDefault-block") private WebElement task_text;
     @FindBy(className="user_avatar") private WebElement profile_button;
-    @FindBy(xpath = "/html/body/main/div/div[2]/div[1]/div[2]/div/span") private WebElement error_login_message;
     @FindBy(className="markdown_content") private WebElement single_task;
     @FindBy(className="markdown_content") private List <WebElement> multiple_task;
-
+/*
+    @FindBy(xpath="//*[@id='__next']/div/main/div[1]/header/nav/div/ul[2]/li[1]/a") private WebElement login_button;
+    @FindBy(xpath="//*[@id='login_form']/button") private WebElement session_button;
+    @FindBy(xpath = "/html/body/main/div/div[2]/div[1]/div[2]/div/span") private WebElement error_login_message;
+*/
 
 
     // Funciones de login y tasks
@@ -58,15 +62,20 @@ public class LoginPage {
     //Metodos de validacion
 
     public boolean validateLogin(){
+
         return profile_button.isDisplayed();
     }
 
     public String validateFailedLogin(){
+
         return error_login_message.getText();
     }
+
     public String validateSingleTask(){
+
         return single_task.getText();
     }
+
     public int validateMultipleTask(){
         System.out.println(multiple_task.size());
         return multiple_task.size();
--- src/test/java/Tests/LoginTest.java
+++ src/test/java/Tests/LoginTest.java
@@ -1,69 +1,70 @@
 package Tests;
 
-import Pages.BasePage;
+
 import Pages.LoginPage;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.OrderWith;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.PageFactory;
+
 
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest{
     WebDriver driver;
     public String baseUrl = "https://todoist.com/es";
-
+    public String pageUser = "jorgebarriba@gmail.com";
+    public String pagePassword = "Challenge02";
+    public String failedUser = "failed@gmail.com";
+    public String failedPassword = "nopass";
+    public String singleTaskName = "Automated Single Task";
+    public String multipleTaskName = "Automated Task Number ";
+    public String errorLoginMessage = "Email o contraseña incorrectos";
 
     @Before
     public void launchBrowser() throws InterruptedException {
-        //String driverPath = "/Users/Jorsh/Downloads/PruebasSelenium/chromedriver3";
         String driverPath = "src/WebDriver/chromedriver3";
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
         driver.get(baseUrl);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
     }
 
 
-
-    @org.junit.Test
+    @Test
     public void SuccessfulLogin(){
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.Login("jorgebarriba@gmail.com", "Challenge02");
+        loginPage.Login(pageUser, pagePassword);
         org.junit.Assert.assertTrue(loginPage.validateLogin());
     }
 
-    @org.junit.Test
+    @Test
     public void UnsuccessfulLogin(){
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.Login("failed@gmail.com", "nopass");
-        org.junit.Assert.assertEquals("Email o contraseña incorrectos",loginPage.validateFailedLogin());
+        loginPage.Login(failedUser, failedPassword);
+        org.junit.Assert.assertEquals(errorLoginMessage,loginPage.validateFailedLogin());
     }
 
-    @org.junit.Test
+   @Test
     public void CreateNewTask(){
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.Login("jorgebarriba@gmail.com", "Challenge02");
-        loginPage.AddTask("Automated Single Task");
-        org.junit.Assert.assertEquals("Automated Single Task",loginPage.validateSingleTask());
+        loginPage.Login(pageUser, pagePassword);
+        loginPage.AddTask(singleTaskName);
+        org.junit.Assert.assertEquals(singleTaskName,loginPage.validateSingleTask());
     }
 
     @Test
     public void Create10Tasks(){
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.Login("jorgebarriba@gmail.com", "Challenge02");
+        loginPage.Login(pageUser, pagePassword);
         loginPage.ClickPlusButton();
         int n = 1;
         while(n<11){
             driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-            loginPage.AddMultipleTask("Automated Task "+n);
+            loginPage.AddMultipleTask(multipleTaskName+n);
             n++;
         }
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
